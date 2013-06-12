bbe200513f782fdecfe95ed36f2e494fb13563ee
Gabe Smedresman <gabesmed@yahoo.com>
2013-06-12 15:47:54 -0700

    clear dist and simplify _onMove handler

--- packages/ember-leaflet/lib/map_view.js
+++ packages/ember-leaflet/lib/map_view.js
@@ -21,11 +21,12 @@ EmberLeaflet.MapView = Ember.View.extend(EmberLeaflet.LayerMixin, {
 
   _createLayer: function() {
     if(this._layer) { return; }
-        
+    Ember.assert("Center and zoom must be set before creating map.",
+      !!this.get('center') && !!this.get('zoom'));
     this._layer = L.map(this.get('elementId'), this.get('options'));
+    this._layer.setView(this.get('center'), this.get('zoom'));
     this._setEventHandlers();
     this._createChildLayers();
-    this.setInitialViewArea();
   },
 
   _destroyLayer: function() {
@@ -51,7 +52,7 @@ EmberLeaflet.MapView = Ember.View.extend(EmberLeaflet.LayerMixin, {
   },
 
   _onZoomEnd: function(e) {
-    this.set('zoom', e.target.getZoom());
+    this.set('zoom', this._layer.getZoom());
   },
 
   _onMoveStart: function(e) {
@@ -63,13 +64,7 @@ EmberLeaflet.MapView = Ember.View.extend(EmberLeaflet.LayerMixin, {
   },
 
   _onMove: function(e) {
-    var newCenter = e.target.getCenter();
-    this.set('center', [newCenter.lat, newCenter.lng]);    
-  },
-
-  setInitialViewArea: function() {
-    if(!this.get('center') || !this.get('zoom')) { return; } 
-    this._layer.setView(this.get('center'), this.get('zoom'));
+    this.set('center', this._layer.getCenter());    
   },
 
   zoomDidChange: Ember.observer(function() {
