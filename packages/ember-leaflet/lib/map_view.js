9504921a3d3685b5d7a358f30ed959b7c9e26be2
miguelcobain <jmandrade0@gmail.com>
2013-06-12 18:00:38 +0100

    Added zoom transition queue. Added map event handlers for updating ember variables (zoom and center).

--- packages/ember-leaflet/lib/map_view.js
+++ packages/ember-leaflet/lib/map_view.js
@@ -20,7 +20,34 @@ EmberLeaflet.MapView = Ember.View.extend(EmberLeaflet.LayerMixin, {
 
   _createLayer: function() {
     if(this._layer) { return; }
+    
+    /*
+     * Queue zoom transitions
+     */
+    if (L.DomUtil.TRANSITION) {
+        L.Map.addInitHook(function() {
+            L.DomEvent.on(this._mapPane, L.DomUtil.TRANSITION_END, function() {
+                var zoom = this._zoomActions.shift();
+                if (zoom !== undefined) {
+                    this.setZoom(zoom);
+                }
+            }, this);
+        });
+    }
+    
+    L.Map.include(!L.DomUtil.TRANSITION ? {} : {
+        _zoomActions : [],
+        queueZoom : function(zoom) {
+            if (this._animatingZoom) {
+                this._zoomActions.push(zoom);
+            } else {
+                this.setZoom(zoom);
+            }
+        }
+    });
+    
     this._layer = L.map(this.get('elementId'), this.get('options'));
+    this._setEventHandlers();
     this._createChildLayers();
     this.setInitialViewArea();
   },
@@ -31,9 +58,39 @@ EmberLeaflet.MapView = Ember.View.extend(EmberLeaflet.LayerMixin, {
     this._layer.remove();
     this._layer = null;
   },
+  
+  _setEventHandlers: function(){
+    var self = this;
+    this._layer.on('zoomend', function(e) {
+      self.set('zoom', e.target.getZoom());
+    });
+    this._layer.on('movestart', function(e) {
+      self.set('moving', true);
+    });
+    this._layer.on('moveend', function(e) {
+      self.set('moving', false);
+    });
+    this._layer.on('move', function(e) {
+      var newCenter = e.target.getCenter();
+      self.set('center', [newCenter.lat,newCenter.lng]);
+    });
+  },
 
   setInitialViewArea: function() {
     if(!this.get('center') || !this.get('zoom')) { return; } 
     this._layer.setView(this.get('center'), this.get('zoom'));
-  }
+  },
+
+  zoomDidChange: function(){
+    if(!this._layer || !this.get('zoom')) { return; }
+    this._layer.queueZoom(this.get('zoom'));
+  }.observes('zoom'),
+  
+  centerDidChange: function(){
+    if(!this._layer || !this.get('center')) { return; }
+    var center = this.get('center');
+    if (!this.get('moving')) {
+      this._layer.panTo(this.get('center'));
+    }
+  }.observes('center')
 });
