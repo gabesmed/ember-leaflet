ed1cd98eaacfa9b2f4873560b84bffd82261f525
Miguel <jmandrade0@gmail.com>
2015-02-24 22:32:43 +0000

    update tests

--- tests/helpers/locations-equal.js
+++ tests/helpers/locations-equal.js
@@ -1,5 +1,5 @@
 //TODO better way to define custom test helpers
-export default function (loc1, loc2) {
+export default function (assert, loc1, loc2) {
   var msg = '', isEqual = false;
   if(!loc1) { msg = 'First location was null'; }
   else if(!loc2) { msg = 'Second location was null'; }
@@ -8,5 +8,5 @@ export default function (loc1, loc2) {
   else if(loc1.lng !== loc2.lng) {
     msg = 'Longitude ' + loc1.lng + ' did not match ' + loc2.lng; }
   else { isEqual = true; }
-  ok(isEqual, msg);
+  assert.ok(isEqual, msg);
 }
--- tests/unit/components/ember-leaflet-test.js
+++ tests/unit/components/ember-leaflet-test.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import {
   moduleForComponent,
-  test
+  test,
 } from 'ember-qunit';
 import locationsEqual from '../../helpers/locations-equal';
 
@@ -19,164 +19,164 @@ var locations = {
   newdelhi: L.latLng(28.635308, 77.22496)
 };
 
-test('it renders', function() {
-  expect(2);
+test('it renders', function(assert) {
+  assert.expect(2);
 
   // creates the component instance
   var component = this.subject();
-  equal(component._state, 'preRender');
+  assert.equal(component._state, 'preRender');
 
   // appends the component to the page
   this.append();
-  equal(component._state, 'inDOM');
+  assert.equal(component._state, 'inDOM');
 });
 
-test('leaflet object should be created', function() {
+test('leaflet object should be created', function(assert) {
   var component = this.subject();
   this.append();
-  ok(component._layer);
-  ok(component._layer._loaded);
-  equal(component.get('element')._leaflet, true);
+  assert.ok(component._layer);
+  assert.ok(component._layer._loaded);
+  assert.equal(component.get('element')._leaflet, true);
 });
 
-test('leaflet object should have default settings', function() {
+test('leaflet object should have default settings', function(assert) {
   var component = this.subject();
   this.append();
-  equal(component.get('zoom'), 16);
-  locationsEqual(component.get('center'), locations.nyc);
+  assert.equal(component.get('zoom'), 16);
+  locationsEqual(assert, component.get('center'), locations.nyc);
 });
 
-test('center and zoom are set on Ember object', function() {
+test('center and zoom are set on Ember object', function(assert) {
   var component = this.subject();
   this.append();
-  locationsEqual(component.get('center'), locations.nyc);
-  equal(component.get('zoom'), 16);
+  locationsEqual(assert, component.get('center'), locations.nyc);
+  assert.equal(component.get('zoom'), 16);
 });
 
-test('center and zoom are set on map', function() {
+test('center and zoom are set on map', function(assert) {
   var component = this.subject();
   this.append();
-  locationsEqual(component._layer.getCenter(), locations.nyc);
-  equal(component._layer.getZoom(), 16);
+  locationsEqual(assert, component._layer.getCenter(), locations.nyc);
+  assert.equal(component._layer.getZoom(), 16);
 });
 
-test('change object center updates map', function() {
+test('change object center updates map', function(assert) {
   var component = this.subject();
   this.append();
   Ember.run(function() {
     component.set('center', locations.sf);
   });
-  locationsEqual(component._layer.getCenter(), locations.sf);
+  locationsEqual(assert, component._layer.getCenter(), locations.sf);
 });
 
-test('change object zoom updates map', function() {
+test('change object zoom updates map', function(assert) {
   var component = this.subject();
   this.append();
   Ember.run(function() {
     component.set('zoom', 17);
   });
-  equal(component._layer.getZoom(), 17);
+  assert.equal(component._layer.getZoom(), 17);
 });
 
-test('change map center updates object', function() {
+test('change map center updates object', function(assert) {
   var component = this.subject();
   this.append();
   component._layer.panTo(locations.sf);
-  locationsEqual(component.get('center'), locations.sf);
+  locationsEqual(assert, component.get('center'), locations.sf);
 });
 
-test('change map zoom updates object', function() {
+test('change map zoom updates object', function(assert) {
   var component = this.subject();
   this.append();
   component._layer.setZoom(17);
-  equal(component.get('zoom'), 17);
+  assert.equal(component.get('zoom'), 17);
 });
 
-test('two zooms in rapid succession end correctly', function() {
+test('two zooms in rapid succession end correctly', function(assert) {
   var component = this.subject();
   this.append();
-  expect(2);
+  assert.expect(2);
   component.set('zoom', 17);
-  stop();
+  var done = assert.async();
   setTimeout(function() {
     component.set('zoom', 18);
     setTimeout(function() {
-      equal(component.get('zoom'), 18, 'zoom correct on object');
-      equal(component._layer.getZoom(), 18, 'zoom correct on map');
-      start();
+      assert.equal(component.get('zoom'), 18, 'zoom correct on object');
+      assert.equal(component._layer.getZoom(), 18, 'zoom correct on map');
+      done();
     }, 100);
   }, 1);
 });
 
-test('default tile layer created', function() {
+test('default tile layer created', function(assert) {
   var component = this.subject();
   this.append();
-  equal(component._childLayers.length, 1, 'should be created');
-  equal(Ember.typeOf(component._childLayers[0]), 'instance',
+  assert.equal(component._childLayers.length, 1, 'should be created');
+  assert.equal(Ember.typeOf(component._childLayers[0]), 'instance',
     'should be instantiated');
-  ok(component._childLayers[0],
+  assert.ok(component._childLayers[0],
     'should be a TileLayer');
-  equal(component._childLayers[0]._layer._map, component._layer,
+  assert.equal(component._childLayers[0]._layer._map, component._layer,
     'should be added to map');
-  equal(component._childLayers[0]._layer._url, '//a.tiles.mapbox.com/v3/examples.map-zr0njcqy/{z}/{x}/{y}.png',
+  assert.equal(component._childLayers[0]._layer._url, '//a.tiles.mapbox.com/v3/examples.map-zr0njcqy/{z}/{x}/{y}.png',
     'should have tileUrl set');
 });
 
-test('don\'t create default layer if childLayers is set', function() {
+test('don\'t create default layer if childLayers is set', function(assert) {
   var component = this.subject();
   component.set('childLayers',[]);
   this.append();
-  equal(component._childLayers.length, 0);
+  assert.equal(component._childLayers.length, 0);
 });
 
-test('_destroyLayer cleans up', function() {
+test('_destroyLayer cleans up', function(assert) {
   var component = this.subject();
   this.append();
   Ember.run(function() {
     component._destroyLayer();
   });
-  equal(component.$('.leaflet-map-pane').length, 0);
-  equal(component.get('element')._leaflet, undefined);
+  assert.equal(component.$('.leaflet-map-pane').length, 0);
+  assert.equal(component.get('element')._leaflet, undefined);
 });
 
-test('zoom events fire', function() {
+test('zoom events fire', function(assert) {
   var component = this.subject();
-  expect(2);
+  assert.expect(2);
+  var done = assert.async();
   component.setProperties({
     zoom: 13,
-    zoomstart: function() { ok(true, 'zoomstart fired'); },
-    zoomend: function() { ok(true, 'zoomend fired'); start();}
+    zoomstart: function() { assert.ok(true, 'zoomstart fired'); },
+    zoomend: function() { assert.ok(true, 'zoomend fired'); done();}
   });
-  stop();
   this.append();
   component._layer.setZoom(14);
 });
 
-test('move events fire', function() {
+test('move events fire', function(assert) {
   var component = this.subject();
-  expect(3);
+  assert.expect(3);
+  var done = assert.async();
   component.setProperties({
     zoom: 13,
-    movestart: function() { ok(true, 'movestart fired'); },
-    move: function() { ok(true, 'move fired'); },
-    moveend: function() { ok(true, 'moveend fired');start(); }
+    movestart: function() { assert.ok(true, 'movestart fired'); },
+    move: function() { assert.ok(true, 'move fired'); },
+    moveend: function() { assert.ok(true, 'moveend fired'); done(); }
   });
-  stop();
   this.append();
   component._layer.setView(locations.paris, 13);
 });
 
-test('click events fire', function() {
+test('click events fire', function(assert) {
   var component = this.subject();
-  expect(2);
+  assert.expect(2);
+  var done = assert.async();
   component.setProperties({
     zoom: 13,
     options: {doubleClickZoom: false},
     center: locations.paris,
-    click: function() { ok(true, 'click fired'); },
-    dblclick: function() { ok(true, 'dblclick fired');start(); }
+    click: function() { assert.ok(true, 'click fired'); },
+    dblclick: function() { assert.ok(true, 'dblclick fired');done(); }
   });
-  stop();
   this.append();
   component._layer.fire('click', {latlng: locations.paris});
   component._layer.fire('dblclick', {latlng: locations.paris});
