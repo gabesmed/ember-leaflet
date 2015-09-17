f54502035cc4c690068fe6d0b2e44bbe8ff72315
VincentDS <de_vinnie111_rulezzz@hotmail.com>
2015-09-17 15:03:39 +0200

    support for ImageOverlay layer

---
+++ addon/layers/image.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+import Layer from './layer';
+import computed from '../utils/computed';
+
+var get = Ember.get;
+
+/**
+ * `imageOverlay` is a layer with a single image.
+ *
+ * @class imageOverlay
+ * @extends Layer
+ */
+
+export default Layer.extend({
+  imageUrl: null,
+  imageBounds: null,
+  options: null,
+
+  _newLayer: function() {
+    return L.imageOverlay(get(this,'imageUrl'), get(this,'imageBounds'), get(this, 'options'));
+  }
+});
